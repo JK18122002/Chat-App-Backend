4d9bcbe1edef77c33d0720a40da8abdf378d44bd
Jitendra Kumar <jitendrasihalinagar5@gmail.com>
2025-09-14 19:23:45 +0530

    sever

--- server.js
+++ server.js
@@ -47,10 +47,9 @@ app.use("/api/messages", messageRouter);
 
 // Connect to MongoDB and start server
 await connectDB();
-if(process.env.NODE_ENV !== "production"){
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
-}
 
-//Export server for render
+//Export
 export default server;
